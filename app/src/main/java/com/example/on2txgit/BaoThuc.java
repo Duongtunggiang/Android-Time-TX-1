4387c73adb3fc96ab562efe560fd949307873e31
DuongAlern <alernwad113@gmail.com>
2024-10-16 00:55:07 +0700

    first commit

---
+++ app/src/main/java/com/example/on2txgit/BaoThuc.java
@@ -0,0 +1,5 @@
+package com.example.on2txgit;
+
+public class BaoThuc {
+
+}
---
+++ app/src/main/java/com/example/on2txgit/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.on2txgit;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.app.TimePickerDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+
+}
